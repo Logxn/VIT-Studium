9167a24a3e369bbbf6a9c4f71a6044255abb55bc
Logan <hello@loganthompson.de>
2020-10-07 19:29:10 +0200

    C# Dec2Bin

---
+++ Technische Informatik/Aufgaben/00_Decimal2Binary/dec2bin.cs
@@ -0,0 +1,34 @@
+/*
+    Author: Logan Thompson
+    Date: 07.10.2020
+    For: github.com/Logxn/VIT-Studium
+    Description: A class that transforms a decimal number to a binary number string
+*/
+using System;
+
+public class Decimal2Binary
+{
+    // We expect a string with the binary number
+    public string Dec2Bin(int dec)
+    {
+        var lastNum = dec;
+        var binaryString = "";
+
+        // We want to divide starting number all the way down until we reach 0
+        while(lastNum != 0)
+        {
+            var remainder = lastNum % 2;
+            lastNum /= 2;
+
+            binaryString += remainder.ToString();
+        }
+
+        // If the calculation above was correct, we should have a binary number in string format
+        // BUT: The string must be reversed first so the binary number is in its correct order
+
+        var charArray = binaryString.ToCharArray();
+        Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+}
